f2aec4c441f8eaa11536b08a6f2eb5888253f52d
Suleman Karigar <37236131+suleman1412@users.noreply.github.com>
2024-05-24 21:48:48 +0530

    updated to make it look better and new sections

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ SOCIAL_MEDIA = {
     "Twitter": "https://twitter.com/suletexh",
 }
 PROJECTS = {
-    "🏆 Schipol Airport Stats - Designed to provide a clear picture of aviation activity at Schiphol Airport": "https://aeroatlas.streamlit.app",
-    "🏆 Reddit Video Downloader Bot - A reddit bot downloads the video and provides a downloadable link for the user. ": "https://github.com/suleman1412/reddit-video-downloader-bot",
+    "🎯 Schipol Airport Stats - Designed to provide a clear picture of aviation activity at Schiphol Airport": "https://aeroatlas.streamlit.app",
+    "🎯 Reddit Video Downloader Bot - A reddit bot downloads the video and provides a downloadable link for the user. ": "https://github.com/suleman1412/reddit-video-downloader-bot",
+}
+
+CERTIFICATES = {
+    "📜 Microsoft Certified: Azure Data Fundamentals": "https://learn.microsoft.com/en-us/users/sulemankarigar-0170/credentials/1a25f5ba53ad474c"
 }
 
 
@@ -56,6 +60,23 @@ with col2:
     )
     st.write("📫", EMAIL)
 
+# --- SIDEBAR ---
+with st.sidebar:
+    # --- Reach out to me ---
+    st.sidebar.header("☎️ Contact Me")
+    contact_form = f"""
+    <form action="https://formsubmit.co/sulemanhsk01@gmail.com" method="POST">
+    <div class="contact-form">
+    <input type="hidden" name="_captcha" value="false">
+    <div class='name'><input type="text" name="name" placeholder="Your name" required style="width: 100%;"></div>
+    <div class='mail'><input type="email" name="email" placeholder="Your email" required style="width: 100%;"></div>
+    <div class='message'><textarea name="message" placeholder="Your message here" required style="width: 100%;"></textarea></div>
+    <div class='button'><button type="submit"> 📤 Send</button></div>
+    </div>
+    </form>
+    """
+    st.sidebar.markdown(contact_form,unsafe_allow_html=True)
+   
 
 # --- SOCIAL LINKS ---
 st.write('\n')
@@ -95,52 +116,54 @@ st.write("---")
 # --- JOB 1
 st.write("🚧", "**Programmer Analyst Trainee | Cognizant**")
 st.write("06/2023 - Present")
-st.write(
+with st.expander("Roles and Responsibilities",expanded=True):
+    st.write(
+        """
+    - ► Automated web scraping at 3 intervals daily, reducing manual data population time by 95%. Integrated Streamlit for efficient data display. 
+    - ► Developed and optimized over 50 customized data feeds using Snowflake and Talend ETL.
+    - ► Collaborated with business analysts to identify client requirements and leveraged Talend ETL tool to design and develop integrated data feeds, reports, and extracts from on-premises and cloud data sources.
     """
-- ► Developed and optimized over 50 customized data feeds using Snowflake and Talend ETL.
-- ► Implemented reusable Talend job components, driving a 30% productivity gain in feed development cycles.
-- ► Collaborated with business analysts to identify client requirements and leveraged Talend ETL tool to design and develop integrated data feeds, reports, and extracts from on-premises and cloud data sources.
-"""
-)
+    )
 
 # --- JOB 2
 st.write('\n')
 st.write("🚧", "**Data Engineering Intern | Cognizant**")
 st.write("01/2023 - 05/2023")
-st.write(
+with st.expander("Roles and Responsibilities",expanded=True):
+    st.write(
+        """
+    - ► Successfully completed an intensive internship program, demonstrating proficiency in Python, SQL, and Microsoft Azure
+    - ► Leveraged newfound skills to optimize database queries and automate data processing.
+    - ► Applied SQL fundamentals to support database management.
+    - ► Acquired foundational knowledge of Microsoft Azure, applying principles to successfully complete a COVID capstone project.
     """
-- ► Successfully completed an intensive internship program, demonstrating proficiency in Python, SQL, and Microsoft Azure
-- ► Leveraged newfound skills to optimize database queries and automate data processing.
-- ► Applied SQL fundamentals to support database management.
-- ► Acquired foundational knowledge of Microsoft Azure, applying principles to successfully complete a COVID capstone project.
-"""
-)
+    )
 
 # --- Projects & Accomplishments ---
 st.write('\n')
-st.subheader("Projects & Accomplishments")
+st.subheader("Education")
+st.write("---")
+st.write("🎓", "**Bachelors of Technology | College of Engineering, Pune**")
+st.write("08/2018 - 05/2022")
+with st.expander("Curriculum", expanded=True):
+    st.write("""Relevant Coursework: Computer Programming, Univariate Calculus, 
+             Ordinary Differential Equations and Multivariate Calculus, 
+             Vector Calculus and Partial Differential Equations, Robotics""")
+    
+# --- Projects & Accomplishments ---
+st.write('\n')
+st.subheader("Projects")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
 
 
+# --- CERTIFICATIONS ---
+st.write('\n')
+st.subheader("Certificates")
+st.write("---")
+for certificates, link in CERTIFICATES.items():
+    st.write(f"[{certificates}]({link})")
 
 
 
-
-with st.sidebar:
-    # --- Reach out to me ---
-    st.sidebar.header("📩 Contact Me")
-    contact_form = f"""
-    <form action="https://formsubmit.co/sulemanhsk01@gmail.com" method="POST">
-    <div class="contact-form">
-    <input type="hidden" name="_captcha" value="false">
-    <div class='name'><input type="text" name="name" placeholder="Your name" required style="width: 100%;"></div>
-    <div class='mail'><input type="email" name="email" placeholder="Your email" required style="width: 100%;"></div>
-    <div class='message'><textarea name="message" placeholder="Your message here" required style="width: 100%;"></textarea></div>
-    <div class='button'><button type="submit"> 📨 Send</button></div>
-    </div>
-    </form>
-    """
-    st.sidebar.markdown(contact_form,unsafe_allow_html=True)
-   
